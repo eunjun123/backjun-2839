18ae8cc4544dc50b64fc98319bda4ae648fc09aa
eunjun123 <113625608+eunjun123@users.noreply.github.com>
2022-12-16 18:57:54 +0900

    초기 커밋.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        int x = int.Parse(input);
+
+        int a = 0;
+        int b = 0;
+
+        while (x > 0)
+        {
+            if (x % 5 == 0)
+            {
+                a = x / 5;
+                break;
+            }
+
+            x -= 3;
+            b++;
+        }
+
+        if (x >= 0)
+            Console.Write(a + b);
+        else
+            Console.Write(-1);
+    }
+}
